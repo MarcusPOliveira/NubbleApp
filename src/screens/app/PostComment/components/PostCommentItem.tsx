cee3ba8a069d66ae3f31af2f7ba45578086c2134
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2023-08-28 23:38:01 -0300

    feat: formatting dates with date-fns

--- src/screens/app/PostComment/components/PostCommentItem.tsx
+++ src/screens/app/PostComment/components/PostCommentItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { PostComment } from '@domain'
+import { dateUtils } from '@utils'
 
 import { Box, ProfileAvatar, Text } from '@components'
 
@@ -12,14 +13,13 @@ export function PostCommentItem({ postComment }: Props) {
   return (
     <Box flexDirection="row" alignItems="center" gap="s12" mb="s16">
       <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box>
+      <Box flex={1}>
         <Text preset="paragraphSmall" bold>
           {postComment.author.userName}
         </Text>
         <Text preset="paragraphSmall" color="gray1">
-          {postComment.message}
+          {postComment.message} - {postComment.createdAtRelative}
         </Text>
-        {/* <Text>{postComment.createdAt}</Text> */}
       </Box>
     </Box>
   )
